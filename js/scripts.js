9660360189c78ed8d0481b6f9a336358f9a31550
anupama.dey <itsdeyanu@gmail.com>
2024-02-20 14:40:18 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-console.log("Welcome to my site! Feel free to explore :)")
+console.log("Welcome to my site! Feel free to explore :)");
+
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
-      x.className += ' responsive';
+        x.className += ' responsive';
     } else {
-      x.className = 'navtoggle';
+        x.className = 'navtoggle';
     }
-  }
+}
